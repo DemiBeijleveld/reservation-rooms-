82e2b9a7ba820419525f24ff71e162e93b32218c
DemiBeijleveld <demi1936@hotmail.com>
2025-03-07 12:32:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-# Email Configuratie (Aanpassen met echte SMTP-gegevens)
-app.config['MAIL_SERVER'] = 'smtp.example.com'  # Gebruik een echte SMTP-server
+# Email Configuratie
+app.config['MAIL_SERVER'] = 'smtp.office365.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'your_email@example.com'
-app.config['MAIL_PASSWORD'] = 'your_password'
+app.config['MAIL_USERNAME'] = 'demi1936@hotmail.com'
+app.config['MAIL_PASSWORD'] = 'Mailvandemi07'
 mail = Mail(app)
 
 # Database Model
